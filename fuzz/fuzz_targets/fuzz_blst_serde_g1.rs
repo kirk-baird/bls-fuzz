0e348a3c4827eca31dcc080398098a6e340c52fb
Kirk Baird <baird.k@outlook.com>
2020-08-04 18:47:11 +1000

    Tidy up TODO
    Signed-off-by: Kirk Baird <baird.k@outlook.com>

--- fuzz/fuzz_targets/fuzz_blst_serde_g1.rs
+++ fuzz/fuzz_targets/fuzz_blst_serde_g1.rs
@@ -3,8 +3,6 @@ use libfuzzer_sys::fuzz_target;
 use blst::min_pk::PublicKey;
 
 fuzz_target!(|data: &[u8]| {
-    // TODO: Point (0, +-2)
-    
     if let Ok(pk) = PublicKey::uncompress(data) {
         let data_round_trip = pk.compress();
         assert_eq!(data.to_vec(), data_round_trip.to_vec());
